d2dbba5d54f3491fc624af4375e0d587aa65893f
Daniel Rodriguez <134450873+drodriguez138@users.noreply.github.com>
2024-04-23 13:36:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function fetchData() {
-    fetch('https://waterdatafortexas.org/reservoirs/basin/rio-grande/recent-conditions.geojson') // Replace with the actual data source URL
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            displayPercentageFull(data);
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-            document.getElementById('dataContainer').textContent = 'Failed to load data.';
-        });
+    fetch('https://waterdatafortexas.org/reservoirs/basin/rio-grande/recent-conditions.geojson', {
+         mode: 'no-cors'
+    }) // Replace with the actual data source URL
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayPercentageFull(data);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+        document.getElementById('dataContainer').textContent = 'Failed to load data.';
+    });
 }
 
 function displayPercentageFull(data) {
